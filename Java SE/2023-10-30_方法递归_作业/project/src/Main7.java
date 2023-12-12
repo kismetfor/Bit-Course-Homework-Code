4105b1731ee8e7e0e8bc92ea75c5ae29eec69f82
Han <kismetfor33455@163.com>
2023-12-12 13:54:05 +0800

    复习了一下汉诺塔

---
+++ Java SE/2023-10-30_方法递归_作业/project/src/Main7.java
@@ -0,0 +1,17 @@
+public class Main7 {
+    //12-12练习一下汉诺塔
+    public static void main(String[] args) {
+        hanoi(3, 'A', 'B', 'C');
+    }
+    public static void hanoi(int n, char source, char auxiliary, char goal){
+        if (n == 1) {
+            System.out.println("把"+n+"从"+source+"移动到"+goal);
+        }
+        else {
+            hanoi(n-1, source, goal,auxiliary);
+            System.out.println("把"+n+"从"+source+"移动到"+goal);
+            hanoi(n-1, auxiliary, source, goal);
+        }
+    }
+
+}
---
+++ Java SE/2023-11-04_封装和包等_作业/project/src/Main1.java
@@ -0,0 +1,10 @@
+public class Main1 {
+    public int aMethod(){
+        static int i = 0;
+        i++;
+        return i;
+    }
+    public static void main(String[] args) {
+        Te
+    }
+}
