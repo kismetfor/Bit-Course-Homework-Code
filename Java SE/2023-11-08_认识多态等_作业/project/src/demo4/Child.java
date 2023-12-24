1a0544f2c9fb5d3954f27b5510523c528950f643
Han <kismetfor33455@163.com>
2023-12-24 15:30:20 +0800

    多态

---
+++ Java SE/2023-11-08_认识多态等_作业/project/src/demo4/Child.java
@@ -0,0 +1,16 @@
+package demo4;
+
+
+public class Child extends Person{
+
+    public String grade;
+
+    public static void main(String[] args){
+
+        Person p = new Child();
+
+        System.out.println(p.name);
+
+    }
+
+}
---
+++ Java SE/2023-11-08_认识多态等_作业/project/src/demo4/Person.java
@@ -0,0 +1,8 @@
+package demo4;
+public class Person{
+
+    private String name = "Person";
+
+    int age=0;
+
+}
