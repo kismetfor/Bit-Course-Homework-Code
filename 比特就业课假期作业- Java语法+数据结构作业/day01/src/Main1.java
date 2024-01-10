d1e411cca1b1fa501b1581a2e385106fb34d4932
kismetfor <111693609+kismetfor@users.noreply.github.com>
2024-01-10 22:45:07 +0800

    开始写bit寒假作业了

---
+++ 比特就业课假期作业- Java语法+数据结构作业/day01/src/Main1.java
@@ -0,0 +1,12 @@
+public class Main1 {
+    /*
+    1、给定一个长度为 n 的字符串，
+    请编写一个函数判断该字符串是否回文。
+    如果是回文请返回true，否则返回false。
+    字符串回文指该字符串正序与其逆序逐字符一致。
+     OJ链接 【难度：简单】
+     */
+    public static void main(String[] args) {
+
+    }
+}
