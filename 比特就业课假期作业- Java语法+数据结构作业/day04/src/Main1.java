b45307c029b266ad9040f4259bcda29b12faf139
kismetfor <111693609+kismetfor@users.noreply.github.com>
2024-01-14 11:53:50 +0800

    选择题day04over

---
+++ 比特就业课假期作业- Java语法+数据结构作业/day04/src/Main1.java
@@ -0,0 +1,41 @@
+public class Main1 {
+    public static void main(String[] args) {
+        int i;
+        for (i = 2; i <= 10; i++) {
+            ;
+        }
+        System.out.println(i);
+    }
+    public static void main3(String[] args) {
+        int x = 3;
+        int y = 1;
+//        if (x=y)
+//            System.out.println("n");
+//        else
+//            System.out.println("e");
+    }
+
+    public static void main2(String[] args) {
+        int x = 0;
+        for (int i = 0; i < 5; i++) {
+            if (i>=2)
+            {
+                x++;
+            }
+        }
+        System.out.println(x);
+    }
+    public static void main1(String[] args) {
+        short a = 0;
+        switch (a) {
+            case 0 :
+                System.out.println("0");
+                break;
+            case 1 :
+                System.out.println("1");
+                break;
+            default:
+                System.out.println("nothing");
+        }
+    }
+}
