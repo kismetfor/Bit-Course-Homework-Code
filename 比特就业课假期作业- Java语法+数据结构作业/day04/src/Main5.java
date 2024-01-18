f60309436cd8886876c31792635503e3af8bb5ea
kismetfor <111693609+kismetfor@users.noreply.github.com>
2024-01-18 21:48:26 +0800

    太强了弗洛伊德

--- 比特就业课假期作业- Java语法+数据结构作业/day04/src/Main5.java
+++ 比特就业课假期作业- Java语法+数据结构作业/day04/src/Main5.java
@@ -1,6 +1,28 @@
 public class Main5 {
     //弗洛伊德循环查找算法
+    public static boolean isHappy(int n) {
+        int slow = n;
+        int fast  = n;
+        while (true) {
+            slow = Next(slow);
+            fast = Next(Next(fast));
+            if (fast == 1) {
+                return true;
+            } else if (fast == slow) {
+                return false;
+            }
+        }
+    }
+    public static int Next(int n){
+        int sum = 0;
+        while (n != 0){
+            int digit = n % 10;
+            sum += digit*digit;
+            n /= 10;
+        }
+        return sum;
+    }
     public static void main(String[] args) {
-
+        System.out.println(isHappy(3));
     }
 }
