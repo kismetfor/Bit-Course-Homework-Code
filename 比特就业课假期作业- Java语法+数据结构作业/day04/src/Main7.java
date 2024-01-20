52194b32f2f195acba81635c507c28c6efc4c09c
kismetfor <111693609+kismetfor@users.noreply.github.com>
2024-01-20 23:38:02 +0800

    丑数空间优化

---
+++ 比特就业课假期作业- Java语法+数据结构作业/day04/src/Main7.java
@@ -0,0 +1,21 @@
+public class Main7 {
+    public static boolean isUgly(int n) {
+
+        while (n != 0 && n != 1) {
+            if (n%2 == 0) {
+                n /= 2;
+            } else if (n%3 == 0) {
+                n /= 3;
+            } else if (n%5 == 0) {
+                n /= 5;
+            } else {
+                return false;
+            }
+        }
+        return n != 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isUgly(17));
+    }
+}
