53107a24742c08e712910ebb2163912144ab90b1
kismetfor <111693609+kismetfor@users.noreply.github.com>
2024-01-20 23:23:50 +0800

    丑数

---
+++ 比特就业课假期作业- Java语法+数据结构作业/day04/src/Main6.java
@@ -0,0 +1,27 @@
+public class Main6 {
+
+    public static boolean isUgly(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        while (true) {
+            if (n%2 == 0) {
+                n /= 2;
+            } else if (n%3 == 0) {
+                n /= 3;
+            } else if (n%5 == 0) {
+                n /= 5;
+            } else if (n==1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isUgly(6));
+        System.out.println(isUgly(1));
+        System.out.println(isUgly(14));
+    }
+}
